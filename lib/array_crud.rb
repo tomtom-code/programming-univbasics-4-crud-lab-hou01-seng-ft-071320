699e5417566b4cd96d7ff4781bb0de3e52d08016
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-02 19:57:40 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,50 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["ha", "grace", "penny", "felicia"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array = ["wow", "I", "am", "really", "learning"]
+element = "arrays!"
+array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["is", "the", "best", "food"]
+  element = "wow"
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+ array = ["wow", "I", "am", "really", "learning"]
+ index_number == 2
+ array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array[5]
 end
 
 def update_element_from_index(array, index_number, element)
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  index_number == 4
+  element = "totally"
+  array[4] = "totally"
 
 end
